be99574d1dfd6c12096857563f7f41823ee42c1e
tina96391 <s102213037@mail1.ncnu.edu.tw>
2016-07-27 10:14:04 +0800

    Add files via upload
    印出金字塔圖案

---
+++ star.java
@@ -0,0 +1,31 @@
+/*
+ * Program Name: star.java
+ * Purpose: 印出金字塔
+		*
+	   *** 
+	  *****
+	 *******
+ * Author: 戴郁庭
+ * Date: 2016/06/30
+ * Last Update: 2016/06/30
+*/
+
+import java.util.*;
+
+public class star{
+	public static void main(String[] args){
+		Scanner in =new Scanner(System.in);
+		System.out.print("輸入金字塔高度:");
+		int n=in.nextInt();
+		
+		for(int i=1;i<=n;i++){
+			for(int k=0;k<n-i;k++)
+				System.out.print(" ");
+			
+			for(int j=0;j<2*i-1;j++)
+				System.out.print("*");
+			
+			System.out.println();
+		}
+	}
+}
